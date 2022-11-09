003cd4f4a62b244d19585fb945a57f3087356616
Jessy <triogaming122@gmail.com>
2022-11-09 11:49:34 -0500

    Client Udp Done

---
+++ ClientUDP/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ClientUDP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendIt = new System.Windows.Forms.Button();
+            this.tbMsg = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSendIt
+            // 
+            this.btnSendIt.Location = new System.Drawing.Point(25, 179);
+            this.btnSendIt.Name = "btnSendIt";
+            this.btnSendIt.Size = new System.Drawing.Size(75, 23);
+            this.btnSendIt.TabIndex = 0;
+            this.btnSendIt.Text = "Send it";
+            this.btnSendIt.UseVisualStyleBackColor = true;
+            this.btnSendIt.Click += new System.EventHandler(this.btnSendIt_Click);
+            // 
+            // tbMsg
+            // 
+            this.tbMsg.Location = new System.Drawing.Point(25, 153);
+            this.tbMsg.Name = "tbMsg";
+            this.tbMsg.Size = new System.Drawing.Size(100, 20);
+            this.tbMsg.TabIndex = 1;
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(25, 87);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPort.TabIndex = 2;
+            // 
+            // tbIp
+            // 
+            this.tbIp.Location = new System.Drawing.Point(25, 48);
+            this.tbIp.Name = "tbIp";
+            this.tbIp.Size = new System.Drawing.Size(100, 20);
+            this.tbIp.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Port :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IP :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Msg:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ClientUDP.Properties.Resources.fat_yoshi;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(410, 236);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbIp);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbMsg);
+            this.Controls.Add(this.btnSendIt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendIt;
+        private System.Windows.Forms.TextBox tbMsg;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ClientUDP/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ClientUDP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool state = false;
+
+        private void btnSendIt_Click(object sender, EventArgs e)
+        {
+
+            UdpClient udp = new UdpClient();
+            int toPort;  //port de destination
+            IPAddress toIP; //ip de destination
+            byte[] msg; //pour l'envoie du message
+            IPEndPoint machineDistante;  //sera composé de l'IP et du port
+
+            if (tbMsg.Text.Length != 0) // Champ pas vide ?
+            {
+                try
+                {
+                    toIP = IPAddress.Parse(tbIp.Text); //Notez l'utilisation de la méthode Parse vs Convert
+                    toPort = int.Parse(tbPort.Text);  //port de destination
+                    machineDistante = new IPEndPoint(toIP, toPort);
+                    //ou pour un broadcast 
+                    //machineDistante = new IPEndPoint(IPAddress.Broadcast, toPort);
+
+                    msg = Encoding.Default.GetBytes(tbMsg.Text);
+                    udp.Send(msg, msg.Length, machineDistante);
+                }
+                catch
+                {
+                    MessageBox.Show("Erreur d'identification de la station");
+                }
+            }
+        }
+    }
+}
